7bb2ba7fc9b636307bd434f613d0025a1cb183b5
chad <chad@816618ff-971e-4f3c-9917-b646dc1eeecf>
2014-03-26 22:06:57 +0000

    Fixed h2 and broke it.
    git-svn-id: http://cdkersey.com/log/2014.03.12/neth2@875 816618ff-971e-4f3c-9917-b646dc1eeecf

--- config.h
+++ config.h
@@ -3,14 +3,15 @@
 
 #include <chdl/chdl.h>
 
-const unsigned W(8),     // Total warps
-               L(8),     // Number of SIMD lanes
-               N(32),    // Number of bits in a machine word
-               R(32),    // Number of registers (and predicate registers)
-               LINE(16); // Words per cache line
+const unsigned W(8),        // Total warps
+               L(8),        // Number of SIMD lanes
+               N(32),       // Number of bits in a machine word
+               R(32),       // Number of registers (and predicate registers)
+               LINE(16),    // Words per cache line
+               ROMSZ(4096); // Instruction ROM size
 
 // Doubled constants mean log base 2 of the corresponding constant. LL bits can
 // hold a lane ID, RR bits can uniquely identify a register.
-const unsigned WW(chdl::CLOG2(W)), RR(chdl::CLOG2(R)), LL(chdl::CLOG2(L));
-
+const unsigned WW(chdl::CLOG2(W)), RR(chdl::CLOG2(R)), LL(chdl::CLOG2(L)),
+               NN(chdl::CLOG2(N));
 #endif
--- h2.cpp
+++ h2.cpp
@@ -16,33 +16,37 @@ void Harmonica2();
 // The pipeline stages
 void Sched(sched_fetch_t &out, splitter_sched_t &in);
 void Fetch(fetch_pred_t &out, sched_fetch_t &in);
-void PredRegs(pred_reg_t &out, fetch_pred_t &in);
-void GpRegs(reg_func_t &out, pred_reg_t &in);
-void Execute(splitter_sched_t &out, reg_func_t &in);
+void PredRegs(pred_reg_t &out, fetch_pred_t &in, splitter_pred_t &wb);
+void GpRegs(reg_func_t &out, pred_reg_t &in, splitter_reg_t &wb);
+void Execute(splitter_sched_t&, splitter_pred_t&, splitter_reg_t&, reg_func_t&);
 
 // Scheduler stage auxiliary functions
 void Starter(splitter_sched_t &out);
 
 // Generic auxiliary functions
-// //
 
+// Implementations
 void Harmonica2() {
   HIERARCHY_ENTER();
 
   // Assemble the pipeline
   sched_fetch_t sf;
-  fetch_pred_t sp;
+  fetch_pred_t fp;
   pred_reg_t pr;
   reg_func_t rx;
   splitter_sched_t xs;
+  splitter_pred_t xp;
+  splitter_reg_t xr;
 
   Sched(sf, xs);
-  Fetch(sp, sf);
-  PredRegs(pr, sp);
-  GpRegs(rx, pr);
-  Execute(xs, rx);
+  Fetch(fp, sf);
+  PredRegs(pr, fp, xp);
+  GpRegs(rx, pr, xr);
+  Execute(xs, xp, xr, rx);
 
-  TAP(sf); TAP(sp); TAP(pr); TAP(rx); TAP(xs);
+  _(sf, "ready") = Lit(1); // TODO: remove this when we complete the cycle
+
+  TAP(sf); TAP(fp); TAP(pr); TAP(rx); TAP(xs);
 }
 
 // Provide the initial warp
@@ -66,17 +70,32 @@ void Sched(sched_fetch_t &out, splitter_sched_t &in) {
   Buffer<WW>(out, buf_in);
 }
 
+// We're foregoing an icache for now and just using a simple instruction ROM
 void Fetch(fetch_pred_t &out, sched_fetch_t &in) {
-  
+  node ready(_(out, "ready"));
+
+  _(in, "ready") = ready;
+  _(out, "valid") = Wreg(ready, _(in, "valid"));
+  _(_(out, "contents"), "warp") = Wreg(ready, _(in, "contents"));
+
+  bvec<CLOG2(ROMSZ)> a(
+    _(_(in, "contents"), "pc")[range<NN-3, (NN-3)+CLOG2(ROMSZ)-1>()]
+  );
+
+  _(_(out, "contents"), "ir") = Wreg(ready,
+    LLRom<CLOG2(ROMSZ), N>(a, "rom.hex")
+  );
 }
 
-void PredRegs(pred_reg_t &out, fetch_pred_t &in) {
+void PredRegs(pred_reg_t &out, fetch_pred_t &in, splitter_pred_t &wb) {
 }
 
-void GpRegs(reg_func_t &out, pred_reg_t &in) {
+void GpRegs(reg_func_t &out, pred_reg_t &in, splitter_reg_t &wb) {
 }
 
-void Execute(splitter_sched_t &out, reg_func_t &in) {
+void Execute(splitter_sched_t &out, splitter_pred_t &pwb, splitter_reg_t &rwb,
+             reg_func_t &in)
+{
 }
 
 int main(int argc, char **argv) {
@@ -84,6 +103,7 @@ int main(int argc, char **argv) {
   Harmonica2();
 
   // Optimization/simulation
+  if (cycdet()) return 1;
   optimize();
 
   ofstream vcd("h2.vcd");
