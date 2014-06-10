c33fd6fa2b6143b1e0cd9ae25d4accc963865aae
chad <chad.d.kersey@gmail.com>
2014-06-10 16:55:51 -0600

    Manually turned the giant or chains into trees.

--- exec.cpp
+++ exec.cpp
@@ -89,63 +89,65 @@ void Execute(splitter_sched_t &out, splitter_pred_t &pwb, splitter_reg_t &rwb,
 }
 
 void RouteFunc(bvec<N_FU> &valid, const reg_func_int_t &in, node in_valid) {
+  HIERARCHY_ENTER();
   harpinst<N, RR, RR> inst(_(in, "ir"));
 
   bvec<N_FU> v;
 
   v[FU_ALU] =
-    inst.get_opcode() == Lit<6>(0x00) || // nop
-    inst.get_opcode() == Lit<6>(0x05) || // neg
-    inst.get_opcode() == Lit<6>(0x06) || // not
-    inst.get_opcode() == Lit<6>(0x07) || // and
-    inst.get_opcode() == Lit<6>(0x08) || // or
-    inst.get_opcode() == Lit<6>(0x09) || // xor
-    inst.get_opcode() == Lit<6>(0x0a) || // add
-    inst.get_opcode() == Lit<6>(0x0b) || // sub
-    inst.get_opcode() == Lit<6>(0x0f) || // shl
-    inst.get_opcode() == Lit<6>(0x10) || // shr
-    inst.get_opcode() == Lit<6>(0x11) || // andi
-    inst.get_opcode() == Lit<6>(0x12) || // ori
-    inst.get_opcode() == Lit<6>(0x13) || // xori
-    inst.get_opcode() == Lit<6>(0x14) || // addi
-    inst.get_opcode() == Lit<6>(0x15) || // subi
-    inst.get_opcode() == Lit<6>(0x19) || // shli
-    inst.get_opcode() == Lit<6>(0x1a) || // shri
-    inst.get_opcode() == Lit<6>(0x25) || // ldi
-    inst.get_opcode() == Lit<6>(0x1f);   // clone
+    ((inst.get_opcode() == Lit<6>(0x00)   || // nop
+      inst.get_opcode() == Lit<6>(0x05)   || // neg
+      inst.get_opcode() == Lit<6>(0x06)   || // not
+      inst.get_opcode() == Lit<6>(0x07)   || // and
+      inst.get_opcode() == Lit<6>(0x08))  || // or
+     (inst.get_opcode() == Lit<6>(0x09)   || // xor
+      inst.get_opcode() == Lit<6>(0x0a)   || // add
+      inst.get_opcode() == Lit<6>(0x0b)   || // sub
+      inst.get_opcode() == Lit<6>(0x0f)   || // shl
+      inst.get_opcode() == Lit<6>(0x10))) || // shr
+    ((inst.get_opcode() == Lit<6>(0x11)   || // andi
+      inst.get_opcode() == Lit<6>(0x12)   || // ori
+      inst.get_opcode() == Lit<6>(0x13)   || // xori
+      inst.get_opcode() == Lit<6>(0x14)   || // addi
+      inst.get_opcode() == Lit<6>(0x15))  || // subi
+     (inst.get_opcode() == Lit<6>(0x19)   || // shli
+      inst.get_opcode() == Lit<6>(0x1a)   || // shri
+      inst.get_opcode() == Lit<6>(0x25)   || // ldi
+      inst.get_opcode() == Lit<6>(0x1f)));   // clone
 
   v[FU_PLU] =
-    inst.get_opcode() == Lit<6>(0x26) || // rtop
-    inst.get_opcode() == Lit<6>(0x2b) || // isneg
-    inst.get_opcode() == Lit<6>(0x2c) || // iszero
-    inst.get_opcode() == Lit<6>(0x27) || // andp
-    inst.get_opcode() == Lit<6>(0x28) || // orp
-    inst.get_opcode() == Lit<6>(0x29) || // xorp
-    inst.get_opcode() == Lit<6>(0x2a);   // notp
+    (inst.get_opcode() == Lit<6>(0x26)  || // rtop
+     inst.get_opcode() == Lit<6>(0x2b)  || // isneg
+     inst.get_opcode() == Lit<6>(0x2c)) || // iszero
+   ((inst.get_opcode() == Lit<6>(0x27)  || // andp
+     inst.get_opcode() == Lit<6>(0x28)) || // orp
+    (inst.get_opcode() == Lit<6>(0x29)  || // xorp
+     inst.get_opcode() == Lit<6>(0x2a)));  // notp
 
   v[FU_MULT] =
     inst.get_opcode() == Lit<6>(0x0c) || // mul
     inst.get_opcode() == Lit<6>(0x16);   // muli
 
   v[FU_DIV] =
-    inst.get_opcode() == Lit<6>(0x0d) || // div
-    inst.get_opcode() == Lit<6>(0x0e) || // mod
-    inst.get_opcode() == Lit<6>(0x17) || // divi
-    inst.get_opcode() == Lit<6>(0x18);   // modi
+    (inst.get_opcode() == Lit<6>(0x0d)  || // div
+     inst.get_opcode() == Lit<6>(0x0e)) || // mod
+    (inst.get_opcode() == Lit<6>(0x17)  || // divi
+     inst.get_opcode() == Lit<6>(0x18));   // modi
 
   v[FU_LSU] =
     inst.get_opcode() == Lit<6>(0x23) || // ld
     inst.get_opcode() == Lit<6>(0x24);   // st
 
   v[FU_BRANCH] =
-    inst.get_opcode() == Lit<6>(0x1b) || // jali
-    inst.get_opcode() == Lit<6>(0x20) || // jalis
-    inst.get_opcode() == Lit<6>(0x1c) || // jalr
-    inst.get_opcode() == Lit<6>(0x21) || // jalrs
-    inst.get_opcode() == Lit<6>(0x1e) || // jmpr
-    inst.get_opcode() == Lit<6>(0x22) || // jmprt
-    inst.get_opcode() == Lit<6>(0x1d) || // jmpi
-    inst.get_opcode() == Lit<6>(0x3a);   // wspawn
+    (inst.get_opcode() == Lit<6>(0x1b)  || // jali
+     inst.get_opcode() == Lit<6>(0x20)  || // jalis
+     inst.get_opcode() == Lit<6>(0x1c)  || // jalr
+     inst.get_opcode() == Lit<6>(0x21)) || // jalrs
+    (inst.get_opcode() == Lit<6>(0x1e)  || // jmpr
+     inst.get_opcode() == Lit<6>(0x22)  || // jmprt
+     inst.get_opcode() == Lit<6>(0x1d)  || // jmpi
+     inst.get_opcode() == Lit<6>(0x3a));   // wspawn
 
   valid = v & bvec<N_FU>(in_valid);
+  HIERARCHY_EXIT();
 }
