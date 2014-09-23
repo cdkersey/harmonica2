8ba8e5be39924d6ca12c8ee1a8134e280096e6ec
chad <chad@cdkersey.com>
2014-09-23 17:39:12 -0400

    Verilog testbench.

--- testbench.v
+++ testbench.v
@@ -1,20 +1,23 @@
+`timescale 1 ns / 1 ns
+
 module top();
-   wire[7:0] cout, cout_prelatch;
+   wire[7:0] cout_prelatch;
    wire cstrobe;
    reg phi;
+   reg [7:0] cout;
    
-   score h2(phi, cout_prelatch, cstrobe);
+   h2 h20(phi, cout_prelatch, cstrobe);
    
    initial begin
         $dumpfile("dump.vcd");
         $dumpvars(2, top);
 	phi = 0;
         cout = 0;
-	#100000 $finish;
+	#1000000 $finish;
      end
 
    always begin
-	#1 phi = ~phi;
+	#5 phi = ~phi;
         if (cstrobe == 1) cout = cout_prelatch;
    end
    
